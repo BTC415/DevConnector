cabdbcce5a9ae038131d6800d314153b8dbaf62e
BTC415 <alenschmitt415@gmail.com>
2024-09-08 05:57:01 +0100

    Added and fixed current user returning functionality

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -111,4 +111,15 @@ router.post('/login', (req, res) => {
 
 })
 
+//@route    GET api/users/
+//@desc     Get current user 
+//@access   Private
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  })
+})
+
 module.exports = router;
