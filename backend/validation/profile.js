9bef6e12f026e2ff6878226b01e0b862b57ed051
BTC415 <alenschmitt415@gmail.com>
2024-09-15 01:59:50 +0100

    Added Get User Profile by handle routing

--- backend/validation/profile.js
+++ backend/validation/profile.js
@@ -4,44 +4,44 @@ const isInputEmpty = require("./is-empty");
 module.exports = function validateProfileInput(data) {
   data.handle = !isInputEmpty(data.handle) ? data.handle : "";
   data.status = !isInputEmpty(data.status) ? data.status : "";
-  data.skills = !isInputEmpty(data.skills) ? data.company : "";
+  data.skills = !isInputEmpty(data.skills) ? data.skills : "";
 
   let errors = {};
   if (!Validator.isLength(data.handle, { min: 2, max: 30 })) {
     errors.handle = "Handle needs to be between 2 and 30 characters."
   }
-  if (!Validator.isEmpty(data.handle)) {
+  if (Validator.isEmpty(data.handle)) {
     errors.handle = "Handle Field is required."
   }
-  if (!Validator.isEmpty(data.skills)) {
+  if (Validator.isEmpty(data.skills)) {
     errors.skills = "Skill Field is required."
   }
-  if (!Validator.isEmpty(data.website)) {
+  if (!isInputEmpty(data.website)) {
     if (!Validator.isURL(data.website)) {
       errors.website = "Invalid URL for website"
     }
   }
-  if (!Validator.isEmpty(data.twitter)) {
+  if (!isInputEmpty(data.twitter)) {
     if (!Validator.isURL(data.twitter)) {
       errors.twitter = "Invalid URL for twitter"
     }
   }
-  if (!Validator.isEmpty(data.facebook)) {
+  if (!isInputEmpty(data.facebook)) {
     if (!Validator.isURL(data.facebook)) {
       errors.facebook = "Invalid URL for facebook"
     }
   }
-  if (!Validator.isEmpty(data.youtube)) {
+  if (!isInputEmpty(data.youtube)) {
     if (!Validator.isURL(data.youtube)) {
       errors.youtube = "Invalid URL for youtube"
     }
   }
-  if (!Validator.isEmpty(data.linkedin)) {
+  if (!isInputEmpty(data.linkedin)) {
     if (!Validator.isURL(data.linkedin)) {
       errors.linkedin = "Invalid URL for linkedin"
     }
   }
-  if (!Validator.isEmpty(data.instagram)) {
+  if (!isInputEmpty(data.instagram)) {
     if (!Validator.isURL(data.instagram)) {
       errors.instagram = "Invalid URL for instagram"
     }
