a92e9c01d94c80bd22ae6af303496617299239e3
BTC415 <alenschmitt415@gmail.com>
2024-09-23 12:37:49 +0100

    Fixed users route for use auth middleware

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const gravatar = require('gravatar');
 const bcryptjs = require('bcryptjs');
-const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const auth = require('../../middleware/auth')
 
